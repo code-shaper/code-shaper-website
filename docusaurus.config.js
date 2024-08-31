03e7588ea7cef38afc3c15ad2bb8a93a5efdffb1
Naresh Bhatia <me@nareshbhatia.dev>
2024-08-30 21:55:11 -0400

    add fathom analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,6 +27,15 @@ const config = {
     locales: ['en'],
   },
 
+  scripts: [
+    // Fathom Analytics (https://usefathom.com)
+    {
+      src: 'https://cdn.usefathom.com/script.js',
+      'data-site': 'XLYPKDBZ',
+      defer: true,
+    },
+  ],
+
   stylesheets: [
     {
       rel: 'preconnect',
